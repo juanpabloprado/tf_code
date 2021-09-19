6aa3333a96623d2e72c6dfc7c98d08ce5b7e5803
Juan Prado <juan@juanpabloprado.com>
2021-09-19 09:04:11 -0500

    Split data out to tfvars file

--- prod.tf
+++ prod.tf
@@ -1,3 +1,22 @@
+variable "whitelist" {
+  type = list(string)
+}
+variable "web_image_id" {
+  type = string
+}
+variable "web_instance_type" {
+  type = string
+}
+variable "web_desired_capacity" {
+  type = string
+}
+variable "web_max_size" {
+  type = string
+}
+variable "web_min_size" {
+  type = string
+}
+
 provider "aws" {
   profile = "default"
   region  = "us-west-2"
@@ -34,19 +53,19 @@ resource "aws_security_group" "prod_web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
 
   tags = {
@@ -72,16 +91,16 @@ resource "aws_elb" "prod_web" {
 
 resource "aws_launch_template" "prod_web" {
   name_prefix   = "prod-web"
-  image_id      = "ami-091dc5ac6b1d84e27"
-  instance_type = "t2.micro"
+  image_id      = var.web_image_id
+  instance_type = var.web_instance_type
 }
 
 resource "aws_autoscaling_group" "prod_web" {
   #  availability_zones  = ["us-west-2a", "us-west-2b"]
   vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
-  desired_capacity    = 1
-  max_size            = 1
-  min_size            = 1
+  desired_capacity    = var.web_desired_capacity
+  max_size            = var.web_max_size
+  min_size            = var.web_min_size
 
   launch_template {
     id      = aws_launch_template.prod_web.id
